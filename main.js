1ba678d84bae81f75dbfb7aa2746de971a03e328
Ghassan Achi <ghassan.achi@gmail.com>
2020-11-27 18:39:08 +0100

    Add unzipping and downloading of asset using the api url instead of browser

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const core = require("@actions/core");
 const { Octokit } = require("@octokit/rest");
+const got = require("got");
+const { pipeline } = require("stream");
+const { createWriteStream, createReadStream, mkdir, mkdtempSync } = require("fs");
+const { promisify } = require("util");
+const { dirname, resolve, join } = require("path");
+const unzipper = require("unzipper");
+const os = require("os");
+
+const asyncPipeline = promisify(pipeline);
+const asyncMkdir = promisify(mkdir);
 
 let owner = core.getInput("owner");
 let repo = core.getInput("repo");
@@ -7,6 +17,9 @@ const excludes = core.getInput("exclude").trim().split(",");
 const assetName = core.getInput("assetName");
 const repository = core.getInput("repository");
 const token = core.getInput("token");
+const assetOutputPath = core.getInput("outputPath");
+const unzipEnabled = core.getInput("unzip");
+const unzipPath = core.getInput("unzipPath");
 
 const octokit = new Octokit({
   auth: token,
@@ -15,6 +28,42 @@ const octokit = new Octokit({
 if (!owner || !repo) {
   [owner, repo] = repository.split("/");
 }
+
+const getAssetLocation = () => {
+  if (!assetName) return undefined;
+
+  if (assetOutputPath) {
+    return resolve(assetOutputPath);
+  }
+  const dir = mkdtempSync(join(os.tmpdir(), "out-"));
+  return join(dir, assetName);
+};
+
+async function downloadGithubAsset(url) {
+  const assetAbsoluteOutputPath = getAssetLocation();
+  const headers = {
+    Accept: "application/octet-stream",
+    "User-Agent": "",
+  };
+
+  await asyncMkdir(dirname(assetAbsoluteOutputPath), { recursive: true });
+
+  await asyncPipeline(
+    got.stream(url, {
+      method: "GET",
+      headers,
+    }),
+    createWriteStream(assetAbsoluteOutputPath)
+  );
+  return assetAbsoluteOutputPath;
+}
+
+const unzipAsset = async (assetPath) => {
+  // Either use the unzipPath if specified or the dirctory of the asset
+  const outPath = resolve(unzipPath || dirname(assetPath));
+  await asyncMkdir(dirname(outPath), { recursive: true });
+  createReadStream(assetPath).pipe(unzipper.Extract({ path: outPath }));
+};
 
 async function run() {
   try {
@@ -43,6 +92,25 @@ async function run() {
         core.setFailed(`Could not find asset ${assetName}`);
         return;
       }
+      try {
+        const url = asset.url.slice(8);
+        const assetPath = await downloadGithubAsset(`https://${token}:@${url}`);
+
+        if (unzipEnabled) {
+          try {
+            await unzipAsset(assetPath);
+            core.setOutput("unzip_success", true);
+          } catch (e) {
+            core.setFailed("Failed to unzip the asset");
+            return;
+          }
+        }
+        core.setOutput("asset_url", asset.url);
+        core.setOutput("asset_path", assetPath);
+      } catch (e) {
+        core.setFailed("Failed to download asset");
+        return;
+      }
     }
 
     if (releases.length) {
@@ -51,7 +119,6 @@ async function run() {
       core.setOutput("release_url", releases[0].url);
       core.setOutput("prerelease", releases[0].prerelease);
       core.setOutput("draft", releases[0].draft);
-      core.setOutput("asset_download_url", releases[0].browser_download_url);
     } else {
       core.setFailed("No valid releases");
     }
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget -v --auth-no-challenge --header='Accept:application/octet-stream' \
+ <auth-url> -O ./tmp/t.zip
